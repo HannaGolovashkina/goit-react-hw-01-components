33f2cbf9aab3963b4c2b320f633cb118767a1d7d
Hanna Golovashkina <108117285+HannaGolovashkina@users.noreply.github.com>
2023-01-16 11:00:38 +0200

    task 1 start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
+import user from "./Profile/user.json";
+import Section from "./Section/Section";
+import Profile from "./Profile/Profile";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+  <div>
+      <Section title={"Task-1 'Social network profile'"}>
+        <Profile
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
+      </Section>
+
     </div>
   );
 };
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,70 @@
+// import user from "./user.json";
+import PropTypes from "prop-types";
+import css from './profile.module.css';
+
+// const userinfo =({username, tag, location, avatar, stats})=> {
+//     return <Profile
+//   username={user.username}
+//   tag={user.tag}
+//   location={user.location}
+//   avatar={user.avatar}
+//   stats={user.stats} />
+// }
+// export default userinfo;
+
+export default function Profile({
+    username,
+    tag,
+    location,
+    avatar,
+    stats: { followers, views, likes },
+  }) {
+    return(
+        // <>
+        // <ProfileCard>
+<div className={css.profile}>
+  <div className={css.description}>
+    <img
+      src={avatar}
+      alt="User avatar"
+      width={100}
+          height={100}
+          className={css.avatar}
+    />
+    <p className={css.name}>{username}</p>
+    <p className={css.tag}>@{tag}</p>
+    <p className={css.location}>{location}</p>
+  </div>
+
+  <ul className={css.stats}>
+    <li className={css.item}>
+      <span className={css.label}>Followers</span>
+      <span className={css.quantity}>{followers}</span>
+    </li>
+    <li className={css.item}>
+      <span className={css.label}>Views</span>
+      <span className={css.quantity}>{views}</span>
+    </li>
+    <li className={css.item}>
+      <span className={css.label}>Likes</span>
+      <span className={css.quantity}>{likes}</span>
+    </li>
+  </ul>
+</div>
+/* </ProfileCard>
+</>) */);
+
+}
+
+Profile.propTypes ={
+    username: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    stars: PropTypes.arrayOf(PropTypes.shape({
+      followers: PropTypes.number,
+      views: PropTypes.number,
+      likes: PropTypes.number,
+  }))}
+
+// export default Profile;
